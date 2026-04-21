45bacefd3299536586395d218a0910a574790b6c
Matheus Oliveira <matheus.6148@gmail.com>
2026-04-21 18:14:31 -0300

    feat: implement UpdateSelfStateTool for agent self-modification and refactor toSystemMessage to SoulEntity
    Signed-off-by: Matheus Oliveira <matheus.6148@gmail.com>

--- src/main/java/dev/omatheusmesmo/qlawkus/agent/AgentService.java
+++ src/main/java/dev/omatheusmesmo/qlawkus/agent/AgentService.java
@@ -2,10 +2,11 @@ package dev.omatheusmesmo.qlawkus.agent;
 
 import dev.langchain4j.service.UserMessage;
 import dev.omatheusmesmo.qlawkus.cognition.SoulEngine;
+import dev.omatheusmesmo.qlawkus.cognition.UpdateSelfStateTool;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import jakarta.enterprise.context.ApplicationScoped;
 
-@RegisterAiService(systemMessageProviderSupplier = SoulEngine.class)
+@RegisterAiService(systemMessageProviderSupplier = SoulEngine.class, tools = UpdateSelfStateTool.class)
 @ApplicationScoped
 @Logged
 public interface AgentService {
