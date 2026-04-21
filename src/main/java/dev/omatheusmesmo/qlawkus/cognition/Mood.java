5170804e9875fb64c9334e8afa18923a31020e98
Matheus Oliveira <matheus.6148@gmail.com>
2026-04-21 13:34:48 -0300

    feat: create SoulEntity in PostgreSQL with coreIdentity, currentState and mood
    Signed-off-by: Matheus Oliveira <matheus.6148@gmail.com>

---
+++ src/main/java/dev/omatheusmesmo/qlawkus/cognition/Mood.java
@@ -0,0 +1,12 @@
+package dev.omatheusmesmo.qlawkus.cognition;
+
+public enum Mood {
+    FOCUSED,
+    CURIOUS,
+    ALERT,
+    REFLECTIVE,
+    ENERGETIC,
+    PLAYFUL,
+    CAUTIOUS,
+    DETERMINED
+}
