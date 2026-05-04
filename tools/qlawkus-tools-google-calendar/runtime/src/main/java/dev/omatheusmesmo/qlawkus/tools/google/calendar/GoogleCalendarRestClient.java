9602869d7c0a79c8e7b060cbd27396a0b9d6f55e
Matheus Oliveira <matheus.6148@gmail.com>
2026-05-04 17:30:24 -0300

    feat(google-calendar): add createEvent method to CalendarTool
    Signed-off-by: Matheus Oliveira <matheus.6148@gmail.com>

--- tools/qlawkus-tools-google-calendar/runtime/src/main/java/dev/omatheusmesmo/qlawkus/tools/google/calendar/GoogleCalendarRestClient.java
+++ tools/qlawkus-tools-google-calendar/runtime/src/main/java/dev/omatheusmesmo/qlawkus/tools/google/calendar/GoogleCalendarRestClient.java
@@ -1,12 +1,15 @@
 package dev.omatheusmesmo.qlawkus.tools.google.calendar;
 
 import dev.omatheusmesmo.qlawkus.tools.google.auth.GoogleAuthHeadersFilter;
+import dev.omatheusmesmo.qlawkus.tools.google.calendar.model.CalendarEvent;
 import dev.omatheusmesmo.qlawkus.tools.google.calendar.model.CalendarEventList;
 import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
 
 @Path("/calendar/v3")
@@ -17,10 +20,16 @@ public interface GoogleCalendarRestClient {
     @GET
     @Path("/calendars/{calendarId}/events")
     CalendarEventList listEvents(
-            @jakarta.ws.rs.PathParam("calendarId") String calendarId,
+            @PathParam("calendarId") String calendarId,
             @QueryParam("timeMin") String timeMin,
             @QueryParam("timeMax") String timeMax,
             @QueryParam("maxResults") Integer maxResults,
             @QueryParam("orderBy") String orderBy,
             @QueryParam("singleEvents") Boolean singleEvents);
+
+    @POST
+    @Path("/calendars/{calendarId}/events")
+    CalendarEvent createEvent(
+            @PathParam("calendarId") String calendarId,
+            CalendarEvent event);
 }
